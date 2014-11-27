51912d370c72f47e96a124235fe57231d42d8037
Christian Dalager <christian@dalager.com>
2014-11-27 15:12:13 +0100

    disabling write protection

--- data/validate_doc_update.js
+++ data/validate_doc_update.js
@@ -1,9 +1,18 @@
-function (newDoc, oldDoc, userCtx) {
-	if (userCtx.roles.indexOf('_admin') !== -1 || userCtx.roles.indexOf('metadata') !== -1) {
+function (newDoc, oldDoc, userCtx) {   
+return;// disable security
+if (userCtx.roles.indexOf('_admin') !== -1 || userCtx.roles.indexOf('metadata') !== -1) {
 		return;
-	} else {
-		throw({
-			forbidden : 'Du skal være logget ind for at kunne gemme.'
-		});
-	}
-}
+	} else {  
+                   if (!newDoc.properties.roles){
+                      throw({forbidden: 'forbudt?'});
+                   }  else  {  if (userCtx.roles.indexOf(newDoc.properties.roles) !== -1 ) {
+		                          return;
+	                        } else {
+		                              throw({
+		                                    	unauthorized:'Du skal være logget ind og autoriseret for at kunne gemme.'
+		                                    });
+	                          }
+                            }    
+			}
+ 			
+ }
